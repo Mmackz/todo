bc78023215274ade380591208988247bd62f1f00
Mmackz <matthewmackenz@gmail.com>
2022-06-15 20:12:22 -0700

    ➕ import DOM module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+import { DOM } from "/src/modules/dom";
+
 // global stylesheets
 import "./reset.css";
 import "./style.css";
 
+// global scripts
+
+
 // import components
 import App from "./components/app";
 
-const root = document.getElementById("root");
+const root = DOM.getEl("#root");
 root.appendChild(App());
