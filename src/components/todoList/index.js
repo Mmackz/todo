c3d22261ee5b8f87b521e7cd1210df79b42b81ed
Mmackz <matthewmackenz@gmail.com>
2022-06-16 18:24:06 -0700

    🔥 remove unused code

--- src/components/todoList/index.js
+++ src/components/todoList/index.js
@@ -1,14 +1,10 @@
 import { DOM } from "/src/modules/dom";
 import { Todo } from "../todo";
-import { Menubar } from "../menubar";
 
 export const ToDoList = (todos) => {
    const todoList = DOM.createEl("ul", "todo-list");
    todos.forEach((todo, index) => {
       todoList.append(Todo(todo, index));
    });
-
-   const menu = Menubar();
-
    return todoList;
 };
