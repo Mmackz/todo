f2c9e3552bc076c06ae9aeb11d042000618ec003
Mmackz <matthewmackenz@gmail.com>
2022-06-17 13:38:22 -0700

    🎨 add placeholder text for input

--- src/components/todoList/index.js
+++ src/components/todoList/index.js
@@ -3,8 +3,13 @@ import { Todo } from "../todo";
 
 export const ToDoList = (todos) => {
    const todoList = DOM.createEl("ul", "todo-list");
-   todos.forEach((todo, index) => {
-      todoList.append(Todo(todo, index));
-   });
+   if (todos.length === 0) {
+      const empty = DOM.createEl("div", ["todo-empty", "todo-text"], "Nothing here...");
+      todoList.append(empty);
+   } else {
+      todos.forEach((todo, index) => {
+         todoList.append(Todo(todo, index));
+      });
+   }
    return todoList;
 };
