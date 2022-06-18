76b91ad180badeabb105ebed13d63bc8b67a7df9
Mmackz <matthewmackenz@gmail.com>
2022-06-17 20:24:07 -0700

    ♻️ consolidate stylesheets

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import { DOM } from "/src/modules/dom";
 
 // global stylesheets
 import "./reset.css";
-import "./style.css";
 
-// global scripts
-
-
-// import components
+// import app component
 import App from "./components/app";
 
 const root = DOM.getEl("#root");
